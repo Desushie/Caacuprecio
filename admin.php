9797f77aee1ea874923b4553f2231530a9234f3a
Desushie <nicognz32@gmail.com>
2026-05-20 01:35:38 +0000

    May 19, 2026, 10:35 PM

--- admin.php
+++ admin.php
@@ -3,6 +3,9 @@ require_once __DIR__ . '/config.php';
 require_admin_or_empresa();
 
 $pdo = db();
+$user = current_user();
+$isEmpresa = is_empresa();
+$myStoreId = $isEmpresa ? (int)($user['tiendas_idtiendas'] ?? 0) : 0;
 
 $stats = [
     'productos' => (int) $pdo->query('SELECT COUNT(*) FROM productos')->fetchColumn(),
